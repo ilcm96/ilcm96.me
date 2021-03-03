2f39e569e4c874ec7957a7de6345da1146303bff
Yun Seongmin <ilcm96@gmail.com>
2021-03-03 18:40:47 +0900

    Add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,81 @@ module.exports = {
         trackingId: `UA-124181509-1`,
       },
     },
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map(edge => {
+                const { siteUrl } = site.siteMetadata
+                const { excerpt, fields, frontmatter } = edge.node
+                const { slug } = fields
+
+                const html = edge.node.html
+                  .replace(/href="\//g, `href="${siteUrl}/`)
+                  .replace(/src="\//g, `src="${siteUrl}/`)
+                  .replace(/"\/static\//g, `"${siteUrl}/static/`)
+                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
+                const url = siteUrl + "/" + slug
+                const postText = `
+                <div style="margin-top=55px; font-style: italic;">(Article posted on ilcm96.me. You can read it online by <a href="${url}">clicking here</a>.)</div>
+                `
+
+                return {
+                  ...frontmatter,
+                  description: excerpt,
+                  date: frontmatter.date,
+                  url,
+                  guid: url,
+                  custom_elements: [
+                    {
+                      "content:encoded": html + postText,
+                    },
+                  ],
+                }
+              }),
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  edges {
+                    node {
+                      excerpt(pruneLength: 250)
+                      html
+                      fields { 
+                        slug   
+                      }
+                      frontmatter {
+                        title
+                        date
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "ilcm96.log RSS Feed",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
